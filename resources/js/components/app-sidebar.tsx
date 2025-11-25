06bb9eeb83870b20e715192d23f91426b3d53319
Luis Isla A <islaluis25@gmail.com>
2025-11-25 12:04:12 -0400

    Items de Navegacion

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -2,64 +2,331 @@ import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from '@/components/ui/sidebar';
 import { dashboard } from '@/routes';
-import { type NavItem } from '@/types';
+import { type MainNavItem, NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import {
+  Apple,
+  CalendarDays,
+  Factory,
+  Files,
+  HandCoins,
+  HandHelping,
+  Handshake,
+  Keyboard,
+  Locate,
+  NotebookPen,
+  ShoppingBasket,
+  SquareChartGantt,
+  TicketPercent,
+  TrendingDown,
+  TrendingUp,
+  Users,
+  Wrench,
+} from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard(),
-        icon: LayoutGrid,
-    },
+  {
+    title: 'Abrir Caja',
+    href: '/cajas/create',
+    icon: Keyboard,
+  },
+];
+
+// Clientes
+
+const mainNavItems2: MainNavItem[] = [
+  {
+    title: 'Empresas',
+    icon: Factory,
+    down: true,
+    subItems: [
+      {
+        title: 'Lista',
+        href: '/ventas',
+      },
+      {
+        title: 'Cotizaciones',
+        href: '/ventas/create',
+      },
+      {
+        title: 'Contratos',
+        href: '/ventas/create',
+      },
+    ],
+  },
+  {
+    title: 'Cronogramas',
+    icon: CalendarDays,
+    href: '/cuentascobrar',
+  },
+
+  {
+    title: 'Mapas',
+    icon: Locate,
+    href: '/cuentascobrar',
+  },
+  {
+    title: 'Seguimientos',
+    icon: NotebookPen,
+    href: '/cuentascobrar',
+  },
+];
+
+// Contabilidad
+
+const mainNavItems3: MainNavItem[] = [
+  {
+    title: 'Cuentas por Cobrar',
+    icon: TrendingUp,
+    href: '/cuentascobrar',
+  },
+  {
+    title: 'Cuentas por Pagar',
+    icon: TrendingDown,
+    subItems: [
+      {
+        title: 'Lista',
+        href: '/cuentaspagar',
+      },
+    ],
+  },
+  {
+    title: 'Compras',
+    icon: ShoppingBasket,
+    down: true,
+    subItems: [
+      {
+        title: 'Realizar Compra',
+        href: '/compras/create',
+      },
+      {
+        title: 'Lista',
+        href: '/compras',
+      },
+    ],
+  },
+
+  {
+    title: 'Ingresos',
+    icon: HandCoins,
+    href: '/ingresos',
+  },
+  {
+    title: 'Retiros',
+    icon: HandHelping,
+    href: '/retiros',
+  },
+  {
+    title: 'Gastos',
+    icon: TicketPercent,
+    href: '/gastos',
+  },
+  {
+    title: 'Productos',
+    icon: Apple,
+    down: true,
+    subItems: [
+      {
+        title: 'Agregar Producto',
+        href: '/productos/create',
+      },
+      //   {
+      //     title: 'Carga Masiva',
+      //     href: '/productos/upload',
+      //   },
+      {
+        title: 'Lista',
+        href: '/productos',
+      },
+      {
+        title: 'Categorias',
+        href: '/categorias',
+      },
+      {
+        title: 'Subcategorias',
+        href: '/subcategorias',
+      },
+      {
+        title: 'Marcas',
+        href: '/marcas',
+      },
+      {
+        title: 'Etiquetas',
+        href: '/etiquetas',
+      },
+    ],
+  },
+  {
+    title: 'Proveedores',
+    icon: Handshake,
+    href: '/proveedores',
+  },
+  {
+    title: 'Inventario',
+    icon: SquareChartGantt,
+    href: '/inventarios',
+  },
+];
+
+// Usuario
+const mainNavItems4: MainNavItem[] = [
+  {
+    title: 'Documentos',
+    icon: Files,
+    href: '/cuentascobrar',
+  },
+
+  {
+    title: 'Usuarios',
+    icon: Users,
+    down: true,
+    subItems: [
+      {
+        title: 'Agregar Usuario',
+        href: '/usuarios/create',
+      },
+      {
+        title: 'Lista',
+        href: '/usuarios',
+      },
+    ],
+  },
+
+  {
+    title: 'Configuraciones',
+    icon: Wrench,
+    down: true,
+    subItems: [
+      {
+        title: 'Roles',
+        href: '/roles',
+      },
+      {
+        title: 'Configs',
+        href: '/configs',
+      },
+    ],
+  },
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
+  // {
+  //     title: 'Repository',
+  //     href: 'https://github.com/laravel/react-starter-kit',
+  //     icon: Folder,
+  // },
+  // {
+  //     title: 'Documentation',
+  //     href: 'https://laravel.com/docs/starter-kits#react',
+  //     icon: BookOpen,
+  // },
 ];
 
 export function AppSidebar() {
-    return (
-        <Sidebar collapsible="icon" variant="inset">
-            <SidebarHeader>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton size="lg" asChild>
-                            <Link href={dashboard()} prefetch>
-                                <AppLogo />
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                </SidebarMenu>
-            </SidebarHeader>
+  return (
+    <Sidebar collapsible="icon" variant="inset">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href={dashboard()} prefetch>
+                <AppLogo />
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
 
-            <SidebarContent>
-                <NavMain items={mainNavItems} />
-            </SidebarContent>
+      <SidebarContent>
+        <NavMain
+          items={mainNavItems}
+          items2={mainNavItems2}
+          items3={mainNavItems3}
+          items4={mainNavItems4}
+        />
+      </SidebarContent>
 
-            <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
-                <NavUser />
-            </SidebarFooter>
-        </Sidebar>
-    );
+      <SidebarFooter>
+        <NavFooter items={footerNavItems} className="mt-auto" />
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
+
+// --------------------------------------------------------------
+// import { NavFooter } from '@/components/nav-footer';
+// import { NavMain } from '@/components/nav-main';
+// import { NavUser } from '@/components/nav-user';
+// import {
+//     Sidebar,
+//     SidebarContent,
+//     SidebarFooter,
+//     SidebarHeader,
+//     SidebarMenu,
+//     SidebarMenuButton,
+//     SidebarMenuItem,
+// } from '@/components/ui/sidebar';
+// import { dashboard } from '@/routes';
+// import { type NavItem } from '@/types';
+// import { Link } from '@inertiajs/react';
+// import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+// import AppLogo from './app-logo';
+
+// const mainNavItems: NavItem[] = [
+//     {
+//         title: 'Dashboard',
+//         href: dashboard(),
+//         icon: LayoutGrid,
+//     },
+// ];
+
+// const footerNavItems: NavItem[] = [
+//     {
+//         title: 'Repository',
+//         href: 'https://github.com/laravel/react-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits#react',
+//         icon: BookOpen,
+//     },
+// ];
+
+// export function AppSidebar() {
+//     return (
+//         <Sidebar collapsible="icon" variant="inset">
+//             <SidebarHeader>
+//                 <SidebarMenu>
+//                     <SidebarMenuItem>
+//                         <SidebarMenuButton size="lg" asChild>
+//                             <Link href={dashboard()} prefetch>
+//                                 <AppLogo />
+//                             </Link>
+//                         </SidebarMenuButton>
+//                     </SidebarMenuItem>
+//                 </SidebarMenu>
+//             </SidebarHeader>
+
+//             <SidebarContent>
+//                 <NavMain items={mainNavItems} />
+//             </SidebarContent>
+
+//             <SidebarFooter>
+//                 <NavFooter items={footerNavItems} className="mt-auto" />
+//                 <NavUser />
+//             </SidebarFooter>
+//         </Sidebar>
+//     );
+// }
--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,37 +1,236 @@
 import {
-    SidebarGroup,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from '@/components/ui/sidebar';
-import { resolveUrl } from '@/lib/utils';
-import { type NavItem } from '@/types';
+import { type MainNavItem, type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@radix-ui/react-collapsible';
+import { ChevronDown } from 'lucide-react';
 
-export function NavMain({ items = [] }: { items: NavItem[] }) {
-    const page = usePage();
-    return (
-        <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Platform</SidebarGroupLabel>
-            <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton
-                            asChild
-                            isActive={page.url.startsWith(
-                                resolveUrl(item.href),
-                            )}
-                            tooltip={{ children: item.title }}
-                        >
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-        </SidebarGroup>
-    );
+interface NavMainProps {
+  items: NavItem[];
+  items2: MainNavItem[];
+  items3: MainNavItem[];
+  items4: MainNavItem[];
 }
+
+interface Props {
+  caja_estado?: string;
+}
+
+export function NavMain({ items, items2, items3, items4 }: NavMainProps) {
+  const { caja_estado } = usePage().props as Props;
+  const page = usePage();
+  return (
+    <SidebarGroup className="px-2 py-0">
+      <SidebarGroupLabel>Cliente</SidebarGroupLabel>
+      <SidebarMenu>
+        {items2.map((item) => (
+          <div>
+            {item.down ? (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={false}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                      <ChevronDown className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.subItems?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild isActive={false}>
+                            <Link href={subItem.href} prefetch>
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ) : (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  asChild
+                  // isActive={page.url.startsWith(
+                  //   typeof item.href === 'string' ? item.href : item.href.url,
+                  // )}
+                  tooltip={{ children: item.title }}
+                >
+                  <Link href={item.href} prefetch>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            )}
+          </div>
+        ))}
+      </SidebarMenu>
+      <SidebarGroupLabel>Contabilidad</SidebarGroupLabel>
+      <SidebarMenu>
+        {items3.map((item) => (
+          <div>
+            {item.down ? (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={false}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                      <ChevronDown className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.subItems?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild isActive={false}>
+                            <Link href={subItem.href} prefetch>
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ) : (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  asChild
+                  // isActive={page.url.startsWith(
+                  //   typeof item.href === 'string' ? item.href : item.href.url,
+                  // )}
+                  tooltip={{ children: item.title }}
+                >
+                  <Link href={item.href} prefetch>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            )}
+          </div>
+        ))}
+      </SidebarMenu>
+      <SidebarGroupLabel>Usuario</SidebarGroupLabel>
+      <SidebarMenu>
+        {items4.map((item) => (
+          <div>
+            {item.down ? (
+              <Collapsible
+                key={item.title}
+                asChild
+                defaultOpen={false}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.title}>
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                      <ChevronDown className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.subItems?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <SidebarMenuSubButton asChild isActive={false}>
+                            <Link href={subItem.href} prefetch>
+                              <span>{subItem.title}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ) : (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  asChild
+                  // isActive={page.url.startsWith(
+                  //   typeof item.href === 'string' ? item.href : item.href.url,
+                  // )}
+                  tooltip={{ children: item.title }}
+                >
+                  <Link href={item.href} prefetch>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            )}
+          </div>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
+
+// -----------------------------------------------------------
+// import {
+//     SidebarGroup,
+//     SidebarGroupLabel,
+//     SidebarMenu,
+//     SidebarMenuButton,
+//     SidebarMenuItem,
+// } from '@/components/ui/sidebar';
+// import { resolveUrl } from '@/lib/utils';
+// import { type NavItem } from '@/types';
+// import { Link, usePage } from '@inertiajs/react';
+
+// export function NavMain({ items = [] }: { items: NavItem[] }) {
+//     const page = usePage();
+//     return (
+//         <SidebarGroup className="px-2 py-0">
+//             <SidebarGroupLabel>Platform</SidebarGroupLabel>
+//             <SidebarMenu>
+//                 {items.map((item) => (
+//                     <SidebarMenuItem key={item.title}>
+//                         <SidebarMenuButton
+//                             asChild
+//                             isActive={page.url.startsWith(
+//                                 resolveUrl(item.href),
+//                             )}
+//                             tooltip={{ children: item.title }}
+//                         >
+//                             <Link href={item.href} prefetch>
+//                                 {item.icon && <item.icon />}
+//                                 <span>{item.title}</span>
+//                             </Link>
+//                         </SidebarMenuButton>
+//                     </SidebarMenuItem>
+//                 ))}
+//             </SidebarMenu>
+//         </SidebarGroup>
+//     );
+// }
